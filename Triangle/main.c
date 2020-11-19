a2a8cebfb9b701f895b01dbe904c165f122dae36
derekjeter9 <64431821+derekjeter9@users.noreply.github.com>
2020-11-19 23:25:08 +0800

    Add files via upload

---
+++ Triangle/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a, b, c, flag = 0;
+    printf("Enter values for a, b and c\n");
+    scanf("%f%f%f", &a, &b, &c);
+
+    if ( a>b )
+    {
+        flag = ((b+c) > a);
+    }
+    else if
+        ( b>c )
+    {
+        flag = ((a+c) > b);
+    }
+    else
+    {
+        flag = ((a+b)>c);
+    }
+    if (flag)
+    {
+        printf("Valid Triangle\n");
+    }
+    else
+    {
+        printf("Invalid Triangle\n");
+    }
+
+    return 0;
+}
